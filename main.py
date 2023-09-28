4d80534f186525cc55894ce4d4b96128ffbcd01a
dady <112051772+Jakee8718@users.noreply.github.com>
2023-09-28 16:19:30 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,37 +3,37 @@ import random
 import string
 from colorama import Fore, Style, init
 
-# Initialize colorama
+# colroama
 init(autoreset=True)
 
-# Function to generate random 4-character username
+# change "k=4" to the number that you want the usernames to be.
 def generate_username():
     characters = string.ascii_lowercase + string.digits
     return ''.join(random.choices(characters, k=4))
 
-# URL for Replit API
+# API
 replit_api_url = 'https://replit.com/data/users/'
 
-# Generate 10000 random usernames
+# Change the number in range(10000): to the number u want it to search.
 for _ in range(10000):
     username = generate_username()
-
-    # Make API request to check if the username exists
+# hi there
+    # API REQUEST
     response = requests.get(replit_api_url + username)
     if response.status_code == 404:
-        # Username is not taken
+        # if user is not taken
         print(f"{Fore.GREEN}Username: {username} - Available{Style.RESET_ALL}")
         with open('valid.txt', 'a') as valid_file:
             valid_file.write(username + '\n')
     elif response.status_code == 200:
-        # Username is taken
+        # if the user is infact taken
         print(f"{Fore.RED}Username: {username} - Taken{Style.RESET_ALL}")
         with open('invalid.txt', 'a') as invalid_file:
             invalid_file.write(username + '\n')
     else:
-        # Handle other response codes as invalid
+        # Nut pls
         print(f"{Fore.RED}Username: {username} - Invalid{Style.RESET_ALL}")
-
+# blah blah
 print("Username checks completed.")
 print("Available usernames saved to valid.txt")
 print("Taken or invalid usernames saved to invalid.txt")
